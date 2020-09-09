4b6d1f4e7e937baba95fe0a35a495c8fbc0c1b69
global <global>
2020-09-08 21:43:25 -0300

    commit

--- AcertijoBatman.js
+++ AcertijoBatman.js
@@ -1,5 +1,5 @@
-let ne = "negro";
-let neDos = "Negro";
+let ne = "batman";
+let neDos = "batman";
 let cont = 0;
 function valor (){
     let rta = document.getElementById("rta").value;
@@ -10,19 +10,19 @@ function valor (){
     } 
     if(rta != ne && cont<1){
         let element = document.querySelector ('#intento')
-        element.innerHTML= 'Respuesta incorrecta, ingrese de nuevo <br/>Intento numero 1';
+        element.innerHTML= '¡MAL! vas a tener que esforzarte <br/><br/>Intento N°1';
     }
     if(rta != ne && cont === 1){
         let element = document.querySelector ('#intento')
-        element.innerHTML= 'Respuesta incorrecta. <br/>Pista: solo hay dos personas en frente y solo dos sombreros blancos. <br/>Intento numero 2';
+        element.innerHTML= 'Incorrecto, ¿Te lo dejé muy dificil? <br/>Pista: Se viste de negro, el sigilo es su aliado. <br/><br/>Intento N°2';
     }
     if(rta != ne && cont === 2){
         let element = document.querySelector ('#intento')
-        element.innerHTML= 'Respuesta incorrecta. <br/>Pista: si la segunda persona, ve un sombrero y solo hay dos blancos, el sombrero que queda adelante es... <br/>Intento numero 3"';
+        element.innerHTML= 'Volviste a fallar, parece que no eres rival <br/>Pista: Fanático de los murciélagos. <br/><br/>Intento N°3';
     }
     if (rta != ne && cont === 3){
         let element = document.querySelector ('#intento')
-        element.innerHTML= 'Respuesta incorrecta. <br/>Ha perdido el juego.<br/> Intento numero 4';
+        element.innerHTML= "Incorrecto <br/> Mejor suerte la próxima vez...si es que la hay<br/>";
         document.getElementById("button").disabled = true;
     } 
     cont++;
